c0350521229e0acf3e5d106c2eb21d5519242cad
Federico Correa <correafedde@gmail.com>
2022-04-23 21:36:10 -0300

    cambios a app y el banner

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-
-
 app.use(express.static('public'));
 
 app.listen(3000, () => {
@@ -10,25 +7,25 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 app.get('/login', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/login.html'))
+    res.sendFile(__dirname + '/views/login.html');
 })
 
 app.get('/register', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/register.html'))
+    res.sendFile(__dirname + '/views/register.html');
 })
 
 app.get('/productCart', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+    res.sendFile(__dirname + '/views/productCart.html');
 })
 
 app.get('/productDetail', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+    res.sendFile(__dirname + '/views/productDetail.html');
 })
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/404.html'))
+    res.sendFile(__dirname + '/views/404.html');
 })
