da45a021af66ce28fa0ef712b09e1a6cce08a720
Federico Correa <correafedde@gmail.com>
2022-04-26 16:30:33 -0300

    arreglos de hreflinks en app y register

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/login.html', (req,res) =>{
+app.get('/login', (req,res) =>{
     res.sendFile(__dirname + '/views/login.html');
 })
 
-app.get('/register.html', (req,res) =>{
+app.get('/register', (req,res) =>{
     res.sendFile(__dirname + '/views/register.html');
 })
 
