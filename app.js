35fc79494143d37d5db5c9c0ebe065c2ebd3b57e
mchapire <marcoschapire@gmail.com>
2022-05-03 17:48:19 -0300

    subido-a-heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.listen(3030, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server start");
 })
 
