7c79b5c3ae65f7d079748cc235ed5476c3d1623f
Federico Correa <correafedde@gmail.com>
2022-07-28 09:06:47 -0300

    backend validator configurado

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -2,15 +2,25 @@ const { body } = require('express-validator');
 const path = require("path");
 
 module.exports = [
-    body('firstName').notEmpty().withMessage('Debes completar el campo de nombre.'),
-    body('lastName').notEmpty().withMessage('Debes completar el campo de apellido.'),
+    body('firstName')
+    .notEmpty().withMessage('Debes completar el campo de nombre.').bail()
+    .isLength({min: 2}).withMessage('Nombre no tiene un minimo de 2 caracteres.'),
+    body('lastName')
+    .notEmpty().withMessage('Debes completar el campo de apellido.').bail()
+    .isLength({min: 2}).withMessage('Apellido no tiene un minimo de 2 caracteres.'),
     body('email')
     .notEmpty().withMessage('Debes completar el campo con un email.').bail()
     .isEmail().withMessage('Debes completar el campo con un email valido.'),
     body('password')
     .notEmpty().withMessage('Debes completar el campo con una contraseña.').bail()
-    .isLength({min: 6}).withMessage('Contraseña no tiene un minimo de 6 caracteres.').bail()
-    .isLength({max: 16}).withMessage('Contraseña tiene mas de 16 caracteres.'),
+    .isStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        returnScore: false})
+    .withMessage('Una contraseña correcta dene tener letras mayúsculas, minúsculas, un número y un carácter especial.'),
     body('confirmPassword')
     .notEmpty().withMessage('Debes completar el campo con la misma contraseña.').bail()
     .custom((value,{req}) =>{
@@ -21,16 +31,14 @@ module.exports = [
     }),
     body('image').custom((value, {req}) => { 
         let file = req.file;
-        let accepetedExtensions = ['.jpg', '.png', '.gif' ];
+        let accepetedExtensions = ['.jpg', 'jpeg','.png', '.gif' ];
     
-        if (!file) {
-            throw new Error('Tiene que subir una imagen');
-        } else {
+        if (file) {
             let fileExtension = path.extname(file.originalname);
             if (!accepetedExtensions.includes(fileExtension)){
                 throw new Error('Las extensiones de archivo pertimidas son: ' + accepetedExtensions.join(', '));
-            }  
-        } 
-        return true    
+            }      
+    }
+    return true 
     })
 ]
